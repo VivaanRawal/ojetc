289688f71dc28f9549536b7f4f84adb577c758e9
VivaanRawal <80296406+VivaanRawal@users.noreply.github.com>
2022-05-17 16:42:02 -0700

    Add files via upload

---
+++ object-detection2/main copy 2.js
@@ -0,0 +1,35 @@
+img="";
+status="";
+function preload(){
+img=loadImage('laptop.jpeg');
+
+}
+function setup(){
+    canvas=createCanvas(640,420);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="status: Detecting Objects";
+}
+function modelLoaded(){
+    console.log("Model Loadded");
+    status=true;
+    objectDetector.detect(img,gotResult);
+}
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+
+}
+function draw(){
+    image(img,0,0,640,420);
+    fill("red");
+    text("Laptop",45,75);
+    noFill();
+    stroke("red");
+    rect(30,60,450,350);
+}
+function back(){
+    window.location="index.html";
+}
---
+++ object-detection2/main.js
@@ -0,0 +1,6 @@
+function Laptop(){
+    window.location="index copy 2.html";
+}
+function TV(){
+    window.location="index copy.html";
+}
